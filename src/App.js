012c1059c7936a5ff4676cf524715b0d471317c2
yujin <yjkimliz03@gmail.com>
2024-11-05 09:44:28 +0900

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Component1 from './Component1';
 import Component2 from './Component2';
 import Component3 from './Component3';
+import Hello from './components/Hello';
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          20241028 react oss class
+          OSS #4-2 React first project 제작하기 (22200150 김유진)
         </p>
+        <Hello />
         <Component1 />
         <Component2 />
         <Component3 />
+
         <a
           className="App-link"
           href="https://reactjs.org"
